9cb1bd833940ce3af45141be99a8de3c95283722
Vivek Kumar Yadav <vivekgj2019@gmail.com>
2026-05-01 14:28:38 +0530

    Working version of the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ import InterviewReport from './pages/InterviewReport'
 import PricingPage from './pages/PricingPage'
 import InterviewHistory from './pages/InterviewHistory'
 
-export const ServerUrl = import.meta.env.VITE_SERVER_URL || "https://prepflow-tj85.onrender.com";
+export const ServerUrl = import.meta.env.VITE_SERVER_URL || (import.meta.env.MODE === 'development' ? import.meta.env.VITE_LOCAL_SERVER_URL : "https://prepflow-tj85.onrender.com");
 
 const App = () => {
   const dispatch = useDispatch()
